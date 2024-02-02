2843cd968b248ee5e0fea433c07fd7dd141e0e8c
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2024-02-02 22:58:48 +0100

    update placeholder DB URL to point to own repo

--- sc_data/constants.py
+++ sc_data/constants.py
@@ -3,6 +3,6 @@ import importlib.resources
 PKG_DB_FILE = "data/spare_cores.db"
 DB_PATH = importlib.resources.files(__name__.split(".")[0]).joinpath(PKG_DB_FILE)
 # FIXME: replace with the final URL of the database
-DB_URL = "https://github.com/nogibjj/Sjg80-Rust-CLI-Binary-with-SQLite/raw/d35d8d9c5ecb6d80322eeeff8df6485a7f09b51b/Car_Database.db"
+DB_URL = "https://github.com/SpareCores/sc-data/raw/main/sc_data/data/spare_cores.db"
 HTTP_TIMEOUT = 5
 DB_REFRESH_SECONDS = 600
