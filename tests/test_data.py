1114f0f40722caaacf6d3f16ee79eebe2050de6b
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-03-13 08:42:20 +0100

    minimal tox for linting and simple tests

---
+++ tests/test_data.py
@@ -0,0 +1,17 @@
+from os.path import abspath, exists
+from sc_data import db
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+
+
+def test_has_path():
+    assert db.path is not None
+    assert exists(db.path)
+
+
+def test_can_query_db():
+    engine = create_engine("sqlite:///" + abspath(db.path))
+    Session = sessionmaker(engine)
+    with Session() as s:
+        servers = s.execute(text("SELECT COUNT(*) FROM server")).fetchone()[0]
+    assert servers > 0
