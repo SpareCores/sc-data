3b5a1b65bcd425722676ff282c52832dcc28b97b
bra-fsn <nagy.attila@gmail.com>
2024-06-27 09:26:46 +0200

    Increase HTTP_TIMEOUT

--- src/sc_data/constants.py
+++ src/sc_data/constants.py
@@ -10,5 +10,5 @@ DB_HASH = (
 )
 # FIXME: replace with the final URL of the database
 DB_URL = "https://sc-data-public-40e9d310.s3.amazonaws.com/sc-data-all.db.bz2"
-HTTP_TIMEOUT = 5
+HTTP_TIMEOUT = 10
 DB_REFRESH_SECONDS = 600
