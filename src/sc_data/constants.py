c3499e3f002462cb5846193db2a50bf2739b47bc
bra-fsn <nagy.attila@gmail.com>
2026-02-20 16:13:53 +0100

    increase http timeout

--- src/sc_data/constants.py
+++ src/sc_data/constants.py
@@ -1,6 +1,6 @@
 DB_BASE_URL = "https://cdn.sparecores.net/sc-data"
 DB_URL = None  # If None, will be constructed from DB_TYPE
 DB_TYPE = "full"  # "full" or "priceless"
-HTTP_TIMEOUT = 30
+HTTP_TIMEOUT = 600
 DB_REFRESH_SECONDS = 600
 DB_CACHE_TTL = 86400  # Cache TTL in seconds (1 day)
